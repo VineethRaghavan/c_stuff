941cc532fb87b0894e1f33312398f3332eaca603
VineethRaghavan <VineethRaghavan@users.noreply.github.com>
2021-06-18 19:16:23 +0530

    Initial commit

---
+++ n_atoi.c
@@ -0,0 +1,16 @@
+#include<stdio.h>
+#include<ctype.h>
+int main()
+{
+	int i, n, sign;
+	char str[10];
+	scanf("%s", str);
+	for(i=0;isspace(str[i]);i++);
+	sign = (str[i] == '-')? -1:1;
+	if (str[i] == '+' || str[i] == '-')
+		i++;
+	for(n=0;isdigit(str[i]);i++)
+		n = 10*n + (str[i] - '0');
+	printf("%d", sign*n);
+	return 0;
+}
